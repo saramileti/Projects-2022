4a42477eac395f05172df8e9b180ba44bee4cd21
Tea Mileti <saramileti11@gmail.com>
2022-07-11 11:55:39 +0200

    Weather App

---
+++ weather_app/scripts/forecast.js
@@ -0,0 +1,27 @@
+const key = 'UJWVGVGHzp6d0Z1wrgKHvD0w6qdX5rHp';
+
+// get weather information
+const getWeather = async (id) => {
+  
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+};
+
+// get city information
+const getCity = async (city) => {
+
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+};
